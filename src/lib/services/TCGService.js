d73a823df30b55ee7417d3a43cb19baa474713e0
Hoi Cheng <hoichengleung@gmail.com>
2025-04-09 21:02:10 +0800

    Sort by earliest set, and show total card count

--- src/lib/services/TCGService.js
+++ src/lib/services/TCGService.js
@@ -19,14 +19,27 @@ export async function fetchPokemonCards(pokemonId) {
         )
     }
 
-    return data.data.map(card => ({
-        id: card.id,
-        name: card.name,
-        image: card.images.large,
-        releaseDate: card.set.releaseDate,
-        setName: card.set.name,
-        setSeriesName: card.set.series,
-        price: getPrice(card.tcgplayer?.prices) ?? null // fallback if no price
+    return data.data
+        .sort((a, b) => {
+            const dateA = new Date(a.set.releaseDate);
+            const dateB = new Date(b.set.releaseDate);
 
-    }));
+            const dateAexist = !isNaN(dateA);
+            const dateBexist = !isNaN(dateB);
+
+            if (!dateAexist && !dateBexist) return 0; //both unavailable, don't order
+            if (!dateAexist) return -1; // A missing, likely old card, shift to front
+            if (!dateBexist) return 1 // B missing, likely old, shift to front
+
+            return dateA - dateB;
+        })
+        .map(card => ({
+            id: card.id,
+            name: card.name,
+            image: card.images.large,
+            releaseDate: card.set.releaseDate,
+            setName: card.set.name,
+            setSeriesName: card.set.series,
+            price: getPrice(card.tcgplayer?.prices) ?? null // fallback if no price
+        }));
 }
--- src/routes/pokemon/[id]/+page.svelte
+++ src/routes/pokemon/[id]/+page.svelte
@@ -8,16 +8,20 @@
 <PokemonHeader pokemon={data.pokemon} />
 </div>
 
-<SectionHeader left="Cards found"/>
+<SectionHeader left="Cards found" right={data.tradingCards.length}/>
 
-<div class="max-w-4xl mx-auto grid grid-cols-2 sm:grid-cols-3 gap-4 py-4 px-1">
+<div class="max-w-4xl mx-auto grid grid-cols-2 sm:grid-cols-3 gap-x-4 gap-y-8 py-4 px-1">
 {#each data.tradingCards as card}
   <div class="rounded-lg">
     <img src="{card.image}" alt="{card.name}" class="w-full h-auto" />
-    <div class="flex flex-col pt-2 pl-2 font-pokemon text-pkd-purple-1 text-2xs">
-      <p>{card.releaseDate.slice(0,4)}</p>
-      <p>{card.setSeriesName}, {card.setName}</p>
-      {#if card.price}<p class="">${card.price}</p>{/if}
+    <div class="flex flex-col pt-3 px-2 font-pokemon text-pkd-purple-1 text-2xs gap-1">  
+      <div class="flex flex-row gap-2">
+        <p>{card.releaseDate.slice(0,4)}</p>
+        <p class="text-pkd-white-a40">•</p>
+        {#if card.price}<p>${card.price}</p>{/if}
+      </div>
+      <p class="text-3xs">{card.setSeriesName}, {card.setName}</p>
+      
     </div>
   </div>
 {/each}
