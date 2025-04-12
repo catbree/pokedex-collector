5fb2880717995ef0a69c2d6aedc14fa6054b6d3a
Hoi Cheng <hoichengleung@gmail.com>
2025-04-12 16:51:07 +0800

    added GA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,15 @@
   <meta name="description" content="A simple tracker for your full National Dex Pokémon card collection." />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="icon" href="/icons/favicon.svg" />
+
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XDK4EXJMBM"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-XDK4EXJMBM');
+  </script>
 </svelte:head>
 
 <div class="bg-pkd-green-3 min-h-screen p-2">
