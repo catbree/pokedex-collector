698ba6642102a24ff0da8d3a7ca3da3375fe2fda
Hoi Cheng <hoichengleung@gmail.com>
2025-04-12 16:29:56 +0800

    Added a wiggle animation and about modal

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,6 +1,7 @@
 <script>
     export let pokedex = [];
     let searchTerm = '';
+    let showInfo = false;
   
     function scrollToPokemon() {
       const match = pokedex.find(p => 
@@ -27,7 +28,9 @@
   </script>
   
   <div class="max-w-4xl mx-auto flex justify-between gap-2 sticky top-0 z-10 backdrop-blur py-2 -mt-2 -pl-2">
-    <img src="/icons/masterball.svg" alt="It's a master ball" class="w-12 h-12" />
+    <button class="cursor-pointer hover:animate-[wiggle_0.3s] hover:bg-pkd-white-a40 hover:rounded-full" on:click={() => {showInfo=true;}}>
+      <img src="/icons/masterball.svg" alt="A masterball." class="w-12 h-12" />
+    </button>
     <div class="flex flex-grow border border-pkd-pink-3 bg-pkd-pink-4 border-2 focus-within:border-white hover:bg-pkd-pink-3">
       <input
       bind:value={searchTerm}
@@ -41,3 +44,25 @@
     </div>
     <button on:click={scrollToTop} class="w-12 h-12 bg-pkd-pink-4 border-2 border-pkd-pink-3 cursor-pointer hover:bg-pkd-pink-3"><img class="w-full h-full" src="/icons/arrow-up-white.svg" alt="arrow-up icon"/></button>
   </div>
+
+  {#if showInfo}
+    <div role="button" tabindex="0" on:keydown={(e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        showInfo = false;
+      }}} 
+      on:click={() => showInfo = false} class="fixed top-0 left-0 w-full h-full z-100 bg-pkd-green-6/90 text-pkd-white-a100 flex items-center justify-center">
+      <div on:click|stopPropagation aria-hidden="true" class="bg-pkd-purple-1 text-pkd-purple-6 font-pokemon text-2xs w-full max-w-xl px-4 sm:px-8 py-4 sm:py-6 rounded m-2 sm:m-4 overflow-y-auto max-h-[90vh]">
+        <div class="flex flex-row justify-between mb-2 items-center">
+          <h2 class="text-sm">ABOUT</h2>
+          <button on:click={() => showInfo = false} class="hover:bg-pkd-purple-2 cursor-pointer"><img class="w-full h-full" src="/icons/cross.svg" alt="cross icon"/></button>
+        </div>
+          <p class="mb-4 text-white border-pkd-pink-3 bg-pkd-pink-4 border-2 p-3">Note: Your progress is saved in this browser only. Clearing your cache will reset everything!</p>
+          <p class="mb-4">If you're looking for something simple to check off your full National Dex collection (currently: 1025) and browse available cards, this may work for you!</p>
+          <p class="mb-4">To start, tap a Pokéball to mark something as collected. Click on a Pokémon to view all of its available cards.</p>
+          <p class="mb-4">
+            Credit goes to <a href="https://developer.tcgplayer.com/" target="_blank" rel="noopener noreferrer" class="underline">TCGplayer API</a> and <a href="https://pokemontcg.io/" target="_blank" rel="noopener noreferrer" class="underline">Pokémon TCG API</a> for data, <a href="https://www.dafont.com/pkmn-rbygsc.font" target="_blank" rel="noopener noreferrer" class="underline">dafont.com</a> for fonts, and <a href="https://www.nintendo.com/" target="_blank" rel="noopener noreferrer" class="underline">Nintendo</a>, <a href="https://www.gamefreak.co.jp/" target="_blank" rel="noopener noreferrer" class="underline">Game Freak</a>, and <a href="https://www.pokemon.com/" target="_blank" rel="noopener noreferrer" class="underline">The Pokémon Company</a> for Pokémon.
+          </p>
+          <p class="mb-2">Passion project built and designed by <a href="https://leunghoicheng.com" target="_blank" rel="noopener noreferrer" class="underline">catbree</p>
+      </div>
+    </div>
+  {/if}
