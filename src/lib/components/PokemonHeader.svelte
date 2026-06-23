9f5a8bd25dcb7fc6b52722dfb8ade953a30ce306
Leung Hoi Cheng <hoichengleung@gmail.com>
2026-06-23 18:01:38 +0800

    restyle pokemon header

--- src/lib/components/PokemonHeader.svelte
+++ src/lib/components/PokemonHeader.svelte
@@ -19,34 +19,34 @@
   
   <div class="flex items-center justify-between p-2">
     <div class="flex items-center space-x-3">
-      <div class="w-12 h-12 border border-pkd-purple-2" style="background: repeating-linear-gradient(
+      <div class="w-12 h-12" style="background: repeating-linear-gradient(
     45deg,
-    var(--color-pkd-purple-1) 0px,
-    var(--color-pkd-purple-1) 4px,
-    var(--color-pkd-purple-2) 4px,
-    var(--color-pkd-purple-2) 8px
+    var(--color-pkd-cream) 0px,
+    var(--color-pkd-cream) 4px,
+    var(--color-pkd-cream-2) 4px,
+    var(--color-pkd-cream-2) 8px
   );">
         <img src="{pokemon.sprite}" alt="{pokemon.name}"/>
       </div>
       <div class="flex flex-col justify-center items-start">
         <span class="relative px-1 leading-none">
-          <span class="z-1 relative inline-block font-pokemon text-xs text-pkd-purple-1 stroke-pkd-purple-3 px-1" style="-webkit-text-stroke: 3px var(--color-pkd-purple-3); paint-order: stroke fill;">{pokemon.id.toString().padStart(4, '0')}</span>
-          <div class="absolute bg-pkd-purple-3 h-[40%] w-full top-[63%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xs"></div>
+          <span class="z-1 relative inline-block font-pokemon text-xs text-pkd-lcd px-1" style="-webkit-text-stroke: 3px var(--color-pkd-green-d); paint-order: stroke fill;">{pokemon.id.toString().padStart(4, '0')}</span>
+          <div class="absolute bg-pkd-green-d border-[2px] border-pkd-green-d h-[55%] w-full top-[62%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xs"></div>
         </span>
-        <p class="pt-1 font-pokemon text-xs text-pkd-purple-6 pb-1">{pokemon.name.toUpperCase()}</p>
+        <p class="pt-1 font-pokemon text-xs text-pkd-ink pb-1">{pokemon.name.toUpperCase()}</p>
       </div>
     </div>
-  
-    <button 
-      class="w-12 h-12 transition rounded-full cursor-pointer"
+
+    <button
+      class="w-14 h-14 transition cursor-pointer"
       on:click|stopPropagation={handleToggle}
     >
       {#if pokemonState === 1}
-        <div class="bg-pkd-white-a0 rounded-full border-1 border-pkd-white-a0"><img src="/icons/pokeball.svg" class="w-full h-full" alt="collected" /></div>
+        <img src="/icons/pokeball.svg" class="w-full h-full" alt="collected" />
       {:else if pokemonState === 2}
-        <div class="bg-pkd-white-a0 rounded-full border-1 border-pkd-white-a0"><img src="/icons/masterball.svg" class="w-full h-full" alt="collected and locked in" /></div>
-      {:else} 
-        <div class="bg-pkd-white-a40 rounded-full border-1 border-dashed border-pkd-purple-2 transition hover:bg-pkd-white-a80"><img src="/icons/noball.svg" class="w-full h-full" alt="not collected" /></div>
+        <img src="/icons/masterball.svg" class="w-full h-full" alt="favourite" />
+      {:else}
+        <img src="/icons/noball.svg" class="w-full h-full opacity-60 transition hover:opacity-100" alt="not collected" />
       {/if}
     </button>
   </div>
