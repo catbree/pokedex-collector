c51540a6cc4035dd7679833f4a7a19db7396fff3
Leung Hoi Cheng <hoichengleung@gmail.com>
2026-06-23 17:55:40 +0800

    restyled sectionheader to new

--- src/lib/components/SectionHeader.svelte
+++ src/lib/components/SectionHeader.svelte
@@ -2,7 +2,7 @@
     export let left = '';
     export let right = '';
 </script>
-<div class="max-w-4xl mx-auto bg-pkd-green-6 flex justify-between p-2 font-pokemon text-xs text-pkd-white-a100">
+<div class="max-w-4xl mx-auto bg-pkd-green flex justify-between p-2 font-pokemon text-xs text-pkd-cream">
     <span>{left}</span>
     {#if right !== null}
         <span>{right}</span>
